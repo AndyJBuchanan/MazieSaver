90c45393c36a1ff1e161831be3237e2b1ba044cc
Andy Buchanan <andy.j.buchanan@googlemail.com>
2015-10-05 23:19:03 +0100

    Initial Commit

---
+++ MazieSaver/MazieSaverView.h
@@ -0,0 +1,13 @@
+//
+//  MazieSaverView.h
+//  MazieSaver
+//
+//  Created by Andy Buchanan on 05/10/2015.
+//  Copyright © 2015 Andy Buchanan. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MazieSaverView : ScreenSaverView
+
+@end
---
+++ MazieSaver/MazieSaverView.m
@@ -0,0 +1,52 @@
+//
+//  MazieSaverView.m
+//  MazieSaver
+//
+//  Created by Andy Buchanan on 05/10/2015.
+//  Copyright © 2015 Andy Buchanan. All rights reserved.
+//
+
+#import "MazieSaverView.h"
+
+@implementation MazieSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
