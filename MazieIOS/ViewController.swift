bbf7488a1a993025dc1d474e179413509b551d78
Andy Buchanan <andy.j.buchanan@googlemail.com>
2019-02-24 17:23:34 +0000

    Created IOS target. Doesn't build yet. Unsurprisingly.

---
+++ MazieIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MazieIOS
+//
+//  Created by Andy Buchanan on 24/02/2019.
+//  Copyright © 2019 Andy Buchanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
